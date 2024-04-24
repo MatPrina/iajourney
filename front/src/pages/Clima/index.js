6a178fa9e8a1dcede9ec67b9bb79935f3705764d
Matheus Prina <matheus.prina2005@gmail.com>
2024-04-24 20:33:13 -0300

    feat(front): adiciona imagens ao clima

--- front/src/pages/Clima/index.js
+++ front/src/pages/Clima/index.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar'
 import lupa from '../../assets/lupa.png'
-import nuvem from '../../assets/clima/04d@2x.png'
+import clearSky from '../../assets/clima/01d@2x.png'
+import fewClouds from '../../assets/clima/02d@2x.png'
+import scatteredClouds from '../../assets/clima/03d@2x.png'
+import brokenClouds from '../../assets/clima/04d@2x.png'
+import showerRain from '../../assets/clima/09d@2x.png'
+import rain from '../../assets/clima/10d@2x.png'
+import thunderStrom from '../../assets/clima/11d@2x.png'
+import snow from '../../assets/clima/13d@2x.png'
+import mist from '../../assets/clima/50d@2x.png'
 import './style.css'
 
+let imgSrc
 const Clima = () => {
 
-    const [grau, setGrau] = useState("??")
+    const [grau, setGrau] = useState("")
     const [dia, setDia] = useState("")
     const [local, setLocal] = useState("")
     const chave = "786066d5d854fd6a91f03ae5c6030bb6"
@@ -25,18 +34,37 @@ const Clima = () => {
     async function weatherApi(localidade) {
         setLocal("")
         const lugar = await geoApi(localidade)
-        console.log(lugar, lugar.lat, lugar.lon)
         const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lugar.lat}&lon=${lugar.lon}&appid=${chave}`)
         .then(res => res.json())
         .catch(err => console.log(err))
         console.log(data)
-        console.log(data.main.temp)
+        if(data.weather[0].description === "clear sky") {
+            imgSrc = clearSky
+        } else if(data.weather[0].description === "few clouds") {
+            imgSrc = fewClouds
+        } else if(data.weather[0].description === "scattered clouds") {
+            imgSrc = scatteredClouds
+        } else if(data.weather[0].description === "broken clouds") {
+            imgSrc = brokenClouds
+        } else if(data.weather[0].description === "shower rain") {
+            imgSrc = showerRain
+        } else if(data.weather[0].description === "rain") {
+            imgSrc = rain
+        } else if(data.weather[0].description === "thunderstorm") {
+            imgSrc = thunderStrom
+        } else if(data.weather[0].description === "snow") {
+            imgSrc = snow
+        } else if(data.weather[0].description === "mist") {
+            imgSrc = mist
+        }
+
         setGrau(Math.floor(data.main.temp - 273.15) + "°")
     }
 
     useEffect(() => {
         const hoje = new Date();
         const diaDaSemana = hoje.getDay();
+        imgSrc = clearSky
 
         switch (diaDaSemana) {
             case 0:
@@ -65,6 +93,7 @@ const Clima = () => {
         }
     }, [])
 
+    
 
     return (
         <div id="clima-container">
@@ -79,7 +108,7 @@ const Clima = () => {
                         <h1 id="clima-response-info-grau">{grau}</h1>
                         <h2 id="clima-response-info-dia">{dia}</h2>
                     </div>
-                    <img src={nuvem} alt="Nuvem" id="clima-response-nuvem"/>
+                    <img src={imgSrc} alt="Nuvem" id="clima-response-nuvem"/>
                 </div>
             </div>
         </div>
